082880b899b971fb792389bec780fa16d09321b5
Mason Keresty <89534948+mkeresty@users.noreply.github.com>
2022-10-18 20:40:46 -0400

    Update and rename useLNRName to useLNRName.ts

--- packages/rainbowkit/src/hooks/useLNRName.ts
+++ packages/rainbowkit/src/hooks/useLNRName.ts
@@ -1,9 +1,10 @@
 import { useMainnet } from './useMainnet';
+import { useLnrName } from '../hooks/useLnrName';
 
 export function useMainnetLnrName(address: string | undefined) {
   const { chainId, enabled } = useMainnet();
 
-  const { data: lnrName } = uselnrName({
+  const { data: lnrName } = useLnrName({
     address,
     chainId,
     enabled,
